8c2c90c6116e6400880901f939c8c80cd882553f
tiennl <ngolethuytien@gmail.com>
2020-09-10 16:14:42 +0700

    bai 6

---
+++ CHUONG 2/BAI 6/ThuaKeThread.java
@@ -0,0 +1,25 @@
+class ThuaKeThread extends Thread {
+    String word;
+    int delay;
+    ThuaKeThread(String s, int d){
+        word =s;
+        delay=d;
+    }
+    public void run(){
+        try{
+            for (int i=0; i<30; i++){
+                System.out.println(word+" ");
+                sleep(delay);
+            }
+        }
+        catch (InterruptedException e) {
+            return;
+        }
+    }
+    public static void main(String[] args) {
+        ThuaKeThread t1 = new ThuaKeThread("ping", 15);
+        t1.start();
+        ThuaKeThread t2 = new ThuaKeThread("PONG", 30);
+        t2.start();
+    }
+}
