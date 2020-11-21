390f272bb85c182cafa3da261d60036687b6afe7
Toan <toan@tabvn.com>
2020-11-21 20:45:22 +0700

    cau 4 chuong 4

---
+++ CHUONG 4/cau4/UDPClient.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+
+public class UDPClient {
+    static final String hostname = "127.0.0.1";
+    static final int port = 8080;
+
+    public static void main(String[] args) {
+        System.out.println("Your are client");
+        try {
+            InetAddress address = InetAddress.getByName(hostname);
+            DatagramSocket socket = new DatagramSocket();
+            Thread receiveThread = new Thread(() -> {
+                while (true) {
+                    try {
+                        byte[] incomingBuffer = new byte[255];
+                        DatagramPacket response = new DatagramPacket(incomingBuffer, incomingBuffer.length);
+                        socket.receive(response);
+                        String receiveMessage = new String(incomingBuffer, 0, response.getLength());
+                        System.out.println("Server says: " + receiveMessage);
+                    } catch (IOException e) {
+                        System.out.println("IO error: " + e.getMessage());
+                    }
+                }
+            });
+
+            Thread sendThread = new Thread(() -> {
+                while (true) {
+                    try {
+                        InputStreamReader isr = new InputStreamReader(System.in); // Nhập
+                        BufferedReader br = new BufferedReader(isr); // một chuỗi
+                        String theString = br.readLine(); // từ bàn phím
+                        byte[] data = theString.getBytes();
+                        DatagramPacket request = new DatagramPacket(data, data.length, address, port);
+                        socket.send(request);
+                    } catch (SocketTimeoutException ex) {
+                        System.out.println("Timeout error: " + ex.getMessage());
+                        ex.printStackTrace();
+                    } catch (IOException ex) {
+                        System.out.println("Client error: " + ex.getMessage());
+                        ex.printStackTrace();
+                    }
+                }
+            });
+            receiveThread.start();
+            sendThread.start();
+        } catch (IOException ex) {
+            System.out.println("Client error: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ CHUONG 4/cau4/UDPServer.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class Client {
+    InetAddress address;
+    int port;
+
+    public Client(InetAddress addr, int port) {
+        this.address = addr;
+        this.port = port;
+    }
+}
+
+public class UDPServer {
+    private DatagramSocket socket;
+    private List<Client> clients = new ArrayList<>();
+
+    public UDPServer(int port) throws SocketException {
+        socket = new DatagramSocket(port);
+    }
+
+    private void run() {
+        System.out.println("Your are server!");
+        Thread receiveThread = new Thread(() -> {
+            while (true) {
+                try {
+                    DatagramPacket request = new DatagramPacket(new byte[255], 255);
+                    socket.receive(request);
+                    String message = new String(request.getData(), 0, request.getLength());
+                    System.out.println("Client says: " + message);
+
+                    InetAddress clientAddress = request.getAddress();
+                    int clientPort = request.getPort();
+                    Client c = new Client(clientAddress, clientPort);
+                    clients.add(c);
+                } catch (IOException e) {
+                    System.out.println("IO error: " + e.getMessage());
+                }
+            }
+        });
+
+        Thread sendThread = new Thread(() -> {
+            while (true) {
+                try {
+                    InputStreamReader isr = new InputStreamReader(System.in); // Nhập
+                    BufferedReader br = new BufferedReader(isr); // một chuỗi
+                    String theString = br.readLine(); // từ bàn phím
+                    byte[] data = theString.getBytes();
+                    for (Client c : clients) {
+                        DatagramPacket response = new DatagramPacket(data, data.length, c.address, c.port);
+                        socket.send(response);
+                    }
+
+                } catch (SocketTimeoutException ex) {
+                    System.out.println("Timeout error: " + ex.getMessage());
+                    ex.printStackTrace();
+                } catch (IOException ex) {
+                    System.out.println("Client error: " + ex.getMessage());
+                    ex.printStackTrace();
+                }
+            }
+        });
+        receiveThread.start();
+        sendThread.start();
+
+
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            UDPServer server = new UDPServer(8080);
+            server.run();
+        } catch (SocketException e) {
+            System.out.println("Socket error: " + e.getMessage());
+        }
+    }
+}
